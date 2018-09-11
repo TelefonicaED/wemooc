d8bcd6783c0b57ccaa68c27df3e55e7b757b566f
Virginia Martín Agudo <virginia.martinagudo@telefonica.com>
2018-09-11 17:51:21 +0200

    Hook para las traducciones.

---
+++ modules/translations-language/src/main/java/com/ted/internal/resource/bundle/LMSResourceBundleES.java
@@ -0,0 +1,29 @@
+package com.ted.internal.resource.bundle;
+
+import com.liferay.portal.kernel.language.UTF8Control;
+
+import java.util.Enumeration;
+import java.util.ResourceBundle;
+
+import org.osgi.service.component.annotations.Component;
+
+@Component(
+	property = { "language.id=es_ES" }, 
+	service = ResourceBundle.class
+)
+public class LMSResourceBundleES extends ResourceBundle {
+
+	@Override
+    protected Object handleGetObject(String key) {
+        return _resourceBundle.getObject(key);
+    }
+
+    @Override
+    public Enumeration<String> getKeys() {
+        return _resourceBundle.getKeys();
+    }
+
+    private final ResourceBundle _resourceBundle = ResourceBundle.getBundle(
+        "content.Language_es", UTF8Control.INSTANCE);
+
+}
